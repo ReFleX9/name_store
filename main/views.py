3bdcb6f125dee6ace8f7430e55f73fdfee2949ca
lisitsynmishka <lisitsynmishka@mail.ru>
2024-05-05 20:24:38 +0300

    5_may

--- main/views.py
+++ main/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from goods.models import Categories
+
 def index(request):
+
+    categories = Categories.objects.all()
+
     context = {
         'title': 'Home - Главная',
-        'content': 'Магазин мебели HOME',
+        'content': 'Магазин спортивного питания',
+        'categories': categories,
     }
     return render(request, 'main/index.html', context)
 
@@ -13,6 +19,6 @@ def about(request):
     context = {
         'title': 'About - о нас',
         'content': 'О нас',
-        'text_on_page': 'Текст о том, какой магазин топ'
+        'text_on_page': 'Текст о том, какой магазин хороший'
     }
     return render(request, 'main/about.html', context)
