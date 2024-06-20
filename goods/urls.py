8f36800d0b3a5c29d52b2724979472118717735f
lisitsynmishka <lisitsynmishka@mail.ru>
2024-06-20 20:22:22 +0300

    new 20 June

--- goods/urls.py
+++ goods/urls.py
@@ -5,6 +5,6 @@ app_name = 'goods'
 
 urlpatterns = [
     path('<slug:category_slug>/', views.catalog, name='index'),
-    path('product/<str:product_slug>/', views.product, name='product'),
+    path('product/<slug:product_slug>/', views.product, name='product'),
     
 ]
