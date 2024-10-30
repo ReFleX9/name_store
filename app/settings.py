4402d33478b8e2342b91b792ba63dea10781aa1e
lisitsynmishka <lisitsynmishka@mail.ru>
2024-10-30 08:16:23 +0300

    30.10.2024

--- app/settings.py
+++ app/settings.py
@@ -40,10 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+
     "debug_toolbar",
 
     'main',
     'goods',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -63,7 +65,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
