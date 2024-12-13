58fac6c302cc90226160876bf5b2163040046cba
lisitsynmishka <lisitsynmishka@mail.ru>
2024-12-13 09:23:25 +0300

    13.12

--- carts/urls.py
+++ carts/urls.py
@@ -4,8 +4,8 @@ from carts.views import cart_add, cart_change, cart_remove
 app_name = 'carts'
 
 urlpatterns = [
-    path('cart_add/<str:product_slug>/', views.cart_add, name='cart_add'),    
-    path('cart_change/<str:product_slug>/', views.cart_change, name='cart_change'),
-    path('cart_remove/<int:cart_id>/', views.cart_remove, name='cart_remove'),
+    path('cart_add/', views.cart_add, name='cart_add'),    
+    path('cart_change/', views.cart_change, name='cart_change'),
+    path('cart_remove/', views.cart_remove, name='cart_remove'),
     
 ]
--- carts/views.py
+++ carts/views.py
@@ -1,11 +1,17 @@
+from urllib import response
 from django.shortcuts import render, redirect
 from goods.models import Products
 from carts.models import Cart
 from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from django.template.loader import render_to_string
+from carts.utils import get_user_carts
 
 
-def cart_add(request, product_slug):
-    product = get_object_or_404(Products, slug=product_slug)
+
+def cart_add(request):
+    product_id = request.POST.get("product_id")
+    product = Products.objects.get(id=product_id)
 
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user, product=product)
@@ -17,13 +23,63 @@ def cart_add(request, product_slug):
                 cart.save()
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
-    return redirect(request.META['HTTP_REFERER'])
 
-def cart_change(request, product_slug):
-    pass
+    user_cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request
+    )
 
-def cart_remove(request, cart_id):
+    response_data = {
+        "message": "Товар добавлен в корзину",
+        "cart_items_html": cart_items_html,
+    }
     
+    return JsonResponse(response_data)
+
+
+def cart_change(request):
+    cart_id = request.POST.get("cart_id")
+    quantity = request.POST.get("quantity")
+
     cart = Cart.objects.get(id=cart_id)
+    
+    cart.quantity = quantity
+    cart.save()
+    updated_quantity = cart.quantity
+
+    cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": cart}, request=request
+    )
+    
+
+    response_data = {
+        "message": "Количество изменено",
+        "cart_items_html": cart_items_html,
+        "quantity_deleted": updated_quantity,
+    }
+    
+    return JsonResponse(response_data)
+
+
+
+
+def cart_remove(request):
+    cart_id = request.POST.get("cart_id")
+
+    cart = Cart.objects.get(id=cart_id)
+    quantity = cart.quantity
     cart.delete()
-    return redirect(request.META['HTTP_REFERER'])
+
+    user_cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request
+    )
+
+    response_data = {
+        "message": "Товар удален",
+        "cart_items_html": cart_items_html,
+        "quantity_deleted": quantity,
+    }
+    
+    return JsonResponse(response_data)
