bb13cb32a82e6ab9594b2abc43847eb95874f638
lisitsynmishka <lisitsynmishka@mail.ru>
2024-12-16 21:11:34 +0300

    16.12

--- orders/models.py
+++ orders/models.py
@@ -1,19 +1,29 @@
-from math import prod
 from django.db import models
 from goods.models import Products
 
 from users.models import User
 
 
+class OrderitemQueryset(models.QuerySet):
+
+    def total_price(self):
+        return sum(cart.products_price() for cart in self)
+    
+    def total_quantity(self):
+        if self:
+            return sum(cart.quantity for cart in self)
+        return 0
+
+
 class Order(models.Model):
-    user = models.ForeignKey(to=User, on_delete=models.SET_DEFAULT, blank=True, null=True, verbose_name="Пользователь")
+    user = models.ForeignKey(to=User, on_delete=models.SET_DEFAULT, default=None, blank=True, null=True, verbose_name="Пользователь")
     created_timestamp = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания заказа")
     phone_number = models.CharField(max_length=20, verbose_name="Номер телефона")
     requires_delivery = models.BooleanField(default=False, verbose_name="Требуетсяы доставка")
     delivery_address = models.TextField(null=True, blank=True, verbose_name="Адрес доставки")
     payment_on_get = models.BooleanField(default=False, verbose_name="Оплата при получении")
     is_paid = models.BooleanField(default=False, verbose_name="Оплачено")
-    status = models.CharField(max_lenght=50, default='В обработке', verbose_name="Статус заказа")
+    status = models.CharField(max_length=50, default='В обработке', verbose_name="Статус заказа")
 
     class Meta:
         db_table = "order"
@@ -25,10 +35,10 @@ class Order(models.Model):
     
 class OrderItem(models.Model):
     order = models.ForeignKey(to=Order, on_delete=models.CASCADE, verbose_name="Заказ")
-    product = models.ForeignKey(Products, on_delete=models.SET_DEFAULT, null=True, verbose_name="Продукт", default="Продукт")
+    product = models.ForeignKey(to=Products, on_delete=models.SET_DEFAULT, null=True, verbose_name="Продукт", default=None)
     name = models.CharField(max_length=150, verbose_name="Название")
     price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name="Цена")
-    quantity = models.PositiveBigIntegerField(default=0, verbose_name="Количество")
+    quantity = models.PositiveIntegerField(default=0, verbose_name="Количество")
     created_timestamp = models.DateTimeField(auto_now_add=True, verbose_name="Дата продажи")
 
     class Meta:
@@ -39,7 +49,7 @@ class OrderItem(models.Model):
     objects = OrderitemQueryset.as_manager()
 
     def products_price(self):
-        return round(self.product.sell_price() * self.quantity, 2)
+        return round(self.price * self.quantity, 2)
     
     def __str__(self):
         return f"Товар {self.name} | Заказ № {self.order.pk}"
--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from orders.admin import OrderTabulareAdmin
 from users.models import User
 from carts.admin import CartTabAdmin
 
@@ -9,4 +10,4 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ["username", "first_name", "last_name", "email"]
     search_fields = ["username", "first_name", "last_name", "email"]
 
-    inlines = [CartTabAdmin,]
+    inlines = [CartTabAdmin, OrderTabulareAdmin]
